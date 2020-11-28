87fab3ec91bafc36838847dbea36ad12b105abdb
anoderpersona <tamibadilla@gmail.com>
2020-11-27 23:56:20 -0300

    Lectura de archivos y formateo de matrices

--- main.hs
+++ main.hs
@@ -1,18 +1,20 @@
 --Lee dos 3 y dos numeros, lee el primer y segundo archivo, sobrescribe el tercero
 import System.Environment --por el getArgs
+import Data.List.Split
 
 main :: IO ()
 main = do
     [fileA,fileB,fileC,bx,by] <- getArgs
-    matrizA <- readFile fileA 
-    matrizB <- readFile fileB
-    --se hace lo que se tiene que hacer y retorna resultado
-
-    --Para que no de error por mientras
-    let resultado = "sdfsdf"
+    archivoA <- readFile fileA 
+    archivoB <- readFile fileB
     --
-    putStrLn matrizA
-    putStrLn matrizB
+    let linesA = lines archivoA
+    let matrizA =  (read (head linesA)::Int) : (read ((head . tail) linesA)::Int) : map (\x -> read x::Int) ((init . splitOn " " . foldl1 (++) . map (\x -> x ++ " ") . tail . tail) linesA)
     --
-
-    --writeFile resultado fileC
+    let linesB = lines archivoB
+    let matrizB =  (read (head linesB)::Int) : (read ((head . tail) linesB)::Int) : map (\x -> read x::Int) ((init . splitOn " " . foldl1 (++) . map (\x -> x ++ " ") . tail . tail) linesB)
+    --
+    
+    print matrizA
+    print matrizB
+    --
