3b56516a676f386c90ac7846db0872362b3825d2
anoderpersona <tamibadilla@gmail.com>
2020-12-07 23:40:59 -0300

    tiempos entrada y salida

--- main.hs
+++ main.hs
@@ -9,12 +9,11 @@ main = do
     principioTotal <- getCPUTime
     --
 
+    principioLectura <- getCPUTime
     --
     [fileA,fileB,fileC,bx,by] <- getArgs
     archivoA <- readFile fileA 
     archivoB <- readFile fileB
-    --
-
     --
     let linesA = lines archivoA
     let matrizA =  (read (head linesA)::Int) : (read ((head . tail) linesA)::Int) : map (\x -> read x::Int) ((init . splitOn " " . foldl1 (++) . map (\x -> x ++ " ") . tail . tail) linesA)
@@ -22,9 +21,16 @@ main = do
     let linesB = lines archivoB
     let matrizB =  (read (head linesB)::Int) : (read ((head . tail) linesB)::Int) : map (\x -> read x::Int) ((init . splitOn " " . foldl1 (++) . map (\x -> x ++ " ") . tail . tail) linesB)
     --
+    finLectura <- getCPUTime
+    let tiempo = (fromIntegral (finLectura - principioLectura))/(10^9)
+    putStr ("El tiempo de entrada en milisegundos es de ")
+    print tiempo
     
+    --
+
     print matrizA
     print matrizB
+    
     --
 
     principioProducto <- getCPUTime
@@ -37,7 +43,18 @@ main = do
     print tiempo
 
     --
-    
+
+    principioEscritura <- getCPUTime
+    --
+    --escritura de archivo
+    --
+    finEscritura <- getCPUTime
+    let tiempo = (fromIntegral (finEscritura - principioEscritura))/(10^9)
+    putStr ("El tiempo de salida en milisegundos es de ")
+    print tiempo
+
+    --
+
     finDeTodo <- getCPUTime
     let tiempo = (fromIntegral (finDeTodo - principioTotal))/(10^9)
     putStr ("El tiempo total en milisegundos es de ")
